e746d5aa0c81091ee62a01a46924a444fec11d0e
artko <artkor05@gmail.com>
2025-10-31 12:35:01 +0500

    create folder analysing_data_and_machine_learning with tasks2_1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tasks.task2_1
+import tasks.analysing_data_and_machine_learning.task2_1.task2_1
 
-def main(): tasks.task2_1.task2_1()
+def main(): tasks.analysing_data_and_machine_learning.task2_1.task2_1.task2_1()
 
 if __name__ == '__main__': main()
