1ff6c7a9c78645b6306bd1443cf939cc243ff55d
artko <artkor05@gmail.com>
2025-11-03 14:02:23 +0500

    little edit main.py in root folder

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import tasks.algorithms_and_complexity_analysis.control_work_2._other.control_work2_task13
 
-"""А можно ли сделать так, чтобы этот файл всегда оставался, но при коммите и отправке не меняется?
-Я сделал здесь какието изменения, но даже при коммите и отправке они всё равно не появлялись в удалённом репозитории.
-Как это сделать?"""
-
-def main():
-    tasks.algorithms_and_complexity_analysis.control_work_2._other.control_work2_task13.task13()
+def main(): tasks.algorithms_and_complexity_analysis.control_work_2._other.control_work2_task13.task13()
 if __name__ == '__main__': main()
